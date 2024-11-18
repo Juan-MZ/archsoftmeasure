57f8c64648e47cdf1bf0c38323779111d68bd902
Juan-MZ <juanjosz@unicauca.edu.co>
2024-11-18 10:51:55 -0500

    Creación del proyecto y repositorio.

---
+++ src/main/java/co/unicauca/archsoftmeasure/ArchsoftmeasureApplication.java
@@ -0,0 +1,13 @@
+package co.unicauca.archsoftmeasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchsoftmeasureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchsoftmeasureApplication.class, args);
+	}
+
+}
